793086a9137d9a77ada6a7bc738bd9a6130e569a
Paul Kramme <pjkramme@gmail.com>
2016-12-04 18:26:47 +0100

    Add chatclient

---
+++ src/chat-client.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netdb.h>
+#include<string.h>
+#include<unistd.h>
+
+
+int main(void)
+{
+	int readcheck = 0;
+	char ipaddress[100];
+	printf("Connect to: ");
+	scanf("%s", ipaddress);
+	printf("\n");
+	int client_socket;
+	char sendline[256];
+	char input[256];
+	struct sockaddr_in servaddr;
+	client_socket = socket(AF_INET, SOCK_STREAM, 0);
+	memset(&servaddr, '\0', sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(8000);
+	inet_pton(AF_INET, ipaddress, &servaddr, sizeof(servaddr));
+	connect(client_socket, (struct sockaddr *) &servaddr, sizeof(servaddr));
+	while(1)
+	{
+		bzero(sendline, 256);
+		/*scanf("%s", sendline);*/
+		getline(sendline, 256, stdin);
+		if(strcmp("/exit", sendline) == 0)
+		{
+			puts("Exiting...");
+			return 0;
+		}
+		puts(sendline);
+		write(client_socket, sendline, strlen(sendline) + 1);
+	}
+}
+		
+		
